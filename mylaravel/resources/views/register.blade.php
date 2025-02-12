88e335d6091e0988f4579ffe662870f22d184a34
Suthasinee-wpk <66160379@go.buu.ac.th>
2025-02-12 21:49:03 +0700

    validate+delete

--- mylaravel/resources/views/register.blade.php
+++ mylaravel/resources/views/register.blade.php
@@ -1,98 +1,147 @@
 @extends('layouts.default')
+
 @section('content')
-<body class="register-page bg-body-secondary">
-    <div class="login-page">
+    <div class="register-page">
         <div class="register-box">
-        <div class="register-logo">
-            <a href="../index2.html"><b>Admin</b>LTE</a>
-        </div>
-        <!-- /.register-logo -->
-        <div class="card">
-            <div class="card-body register-card-body">
-            <p class="register-box-msg">Register a new membership</p>
-            <form action="{{url('/register')}}" method="post">
-                @csrf
-                <div class="input-group mb-3">
-                <input type="text" name="name" class="form-control" placeholder="Full Name">
-                <div class="input-group-text"><span class="bi bi-person"></span></div>
-                </div>
-                <div class="input-group mb-3">
-                <input type="email" name="email" class="form-control" placeholder="Email">
-                <div class="input-group-text"><span class="bi bi-envelope"></span></div>
-                </div>
-                <div class="input-group mb-3">
-                <input type="password" name="password" class="form-control" placeholder="Password">
-                <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
-                </div>
-                <!--begin::Row-->
-                <div class="row">
-                <div class="col-8">
-                    <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                    <label class="form-check-label" for="flexCheckDefault">
-                        I agree to the <a href="#">terms</a>
-                    </label>
-                    </div>
-                </div>
-                <!-- /.col -->
-                <div class="col-4">
-                    <div class="d-grid gap-2">
-                    <button type="submit" class="btn btn-primary">Sign In</button>
-                    </div>
-                </div>
-                <!-- /.col -->
-                </div>
-                <!--end::Row-->
-            </form>
-            <div class="social-auth-links text-center mb-3 d-grid gap-2">
-                <p>- OR -</p>
-                <a href="#" class="btn btn-primary">
-                <i class="bi bi-facebook me-2"></i> Sign in using Facebook
-                </a>
-                <a href="#" class="btn btn-danger">
-                <i class="bi bi-google me-2"></i> Sign in using Google+
-                </a>
+            <div class="register-logo">
+                <a href="../index2.html"><b>Admin</b>LTE</a>
             </div>
-            <!-- /.social-auth-links -->
-            <p class="mb-0">
-                <a href="login.html" class="text-center"> I already have a membership </a>
-            </p>
+            <!-- /.register-logo -->
+            <div class="card">
+                <div class="card-body register-card-body">
+                    <p class="register-box-msg">Register a new membership</p>
+                    <form action="{{ url('/register') }}"  method="post">
+                        @csrf <!-- ทุกครั้งที่ทำฟอร์มให้ใส่ด้วยไม่งั้นข้อมูลจะไม่ส่งค่าไป -->
+                        <div class="input-group mb-3">
+                            <input type="text" name="name" id="name" class="form-control"
+                                placeholder="Full Name" />
+                            <div class="input-group-text"><span class="bi bi-person"></span></div>
+                            <div class="valid-feedback">
+                                OK
+                            </div>
+                            <div class="invalid-feedback" id="invalid-name">
+                                กรุณาระบุข้อมูล name
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="email" name="email" id="email" class="form-control" placeholder="Email"/>
+                            <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+                            <div class="valid-feedback">
+                                OK
+                            </div>
+                            <div class="invalid-feedback" id="invalid-email">
+                                กรุณาระบุข้อมูล email
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" name="password" id="pass" class="form-control"
+                                placeholder="Password" />
+                            <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                            <div class="valid-feedback">
+                                OK
+                            </div>
+                            <div class="invalid-feedback" id="invalid-pass">
+                                กรุณาระบุข้อมูล pass
+                            </div>
+                        </div>
+                        <!--begin::Row-->
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="form-check">
+                                    <input class="form-check-input" id="mycheckbox" type="checkbox" value=""
+                                        id="flexCheckDefault" />
+                                    <label class="form-check-label" for="flexCheckDefault">
+                                        I agree to the <a href="#">terms</a>
+                                    </label>
+                                </div>
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-4">
+                                <div class="d-grid gap-2">
+                                    <button type="submit" class="btn btn-primary">Sign In</button>
+                                </div>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!--end::Row-->
+                    </form>
+                    <button class="btn" onclick="myfunction()">Click Me</button>
+                    <div class="social-auth-links text-center mb-3 d-grid gap-2">
+                        <p>- OR -</p>
+                        <a href="#" class="btn btn-primary">
+                            <i class="bi bi-facebook me-2"></i> Sign in using Facebook
+                        </a>
+                        <a href="#" class="btn btn-danger">
+                            <i class="bi bi-google me-2"></i> Sign in using Google+
+                        </a>
+                    </div>
+                    <!-- /.social-auth-links -->
+                    <p class="mb-0">
+                        <a href="login.html" class="text-center"> I already have a membership </a>
+                    </p>
+                </div>
+                <!-- /.register-card-body -->
             </div>
-            <!-- /.register-card-body -->
         </div>
     </div>
-</div>
-    <!-- /.register-box -->
-    <!--begin::Third Party Plugin(OverlayScrollbars)-->
-    <script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js" integrity="sha256-dghWARbRe2eLlIJ56wNB+b760ywulqK3DzZYEpsg2fQ=" crossorigin="anonymous"></script>
-    <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-    <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-    <script src="../../../dist/js/adminlte.js"></script>
-    <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
+@endsection
+
+@section('scripts')
     <script>
-      const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
-      const Default = {
-        scrollbarTheme: 'os-theme-light',
-        scrollbarAutoHide: 'leave',
-        scrollbarClickScroll: true,
-      };
-      document.addEventListener('DOMContentLoaded', function () {
-        const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
-        if (sidebarWrapper && typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== 'undefined') {
-          OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
-            scrollbars: {
-              theme: Default.scrollbarTheme,
-              autoHide: Default.scrollbarAutoHide,
-              clickScroll: Default.scrollbarClickScroll,
-            },
-          });
+        // let $myval
+        // var myval2 = "value of myval2"
+        // const myval3 = ""
+        // console.log("Hello World!")
+        // alert("Hello World!")
+        /**/
+        //
+        // ALERT("Hello World!")
+
+        function myfunction() {
+            let name = $('#name').val();
+            let email = $('#email').val();
+            let pass = $('#pass').val();
+            let mycheckbox = $('#mycheckbox').prop('checked');
+            // document.getElementsByClass()
+            // name.value = "My Name Value"
+            // name.val("My Name Value")
+            console.log(name, email, pass, mycheckbox);
+
+            if (name == "") {
+                $('#name').addClass('is-invalid');
+                $('#invalid-name').html("<b><u>กรุณาระบุชื่อ</u></b>");
+                return false;
+            } else {
+                $('#name').removeClass('is-invalid');
+            }
+
+            let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            if (!emailPattern.test(email)) {
+                $('#email').addClass('is-invalid');
+                $('#invalid-email').html("<b><u> กรุณาระบุอีเมลให้ถูกต้อง @ และ . </u></b>");
+                return false;
+            } else {
+                $('#email').removeClass('is-invalid');
+            }
+
+            let passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/;
+            if (!passwordPattern.test(pass)) {
+                $('#pass').addClass('is-invalid');
+                $('#invalid-pass').html("<b><u> รหัสผ่านต้องมีตัวอักษรพิมพ์ใหญ่ ตัวอักษรพิมพ์เล็ก และตัวเลข </u></b>");
+                return false;
+            } else {
+                $('#pass').removeClass('is-invalid');
+            }
+
+            if (!mycheckbox) {
+                $('mycheckbox').addClass('is-invalid');
+                alert('กรุณายอมรับข้อกำหนด');
+                return false;
+            }
+            return true;
         }
-      });
     </script>
-    <!--end::OverlayScrollbars Configure-->
-    <!--end::Script-->
-</body>
+    <script>
+        //console.log(myval2);
+    </script>
 @endsection
--- mylaravel/resources/views/user/index.blade.php
+++ mylaravel/resources/views/user/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.default_with_menu')
 
 @section('content')
+<style>
+ table tbody tr:hover td {
+    color: rgb(151, 12, 250);
+    background-color: rgba(168, 168, 255, 0.349);
+}
+
+</style>
 <div class="row">
     <div class="col-md-12">
       <div class="card mb-12">
@@ -26,7 +33,7 @@
                     <a href="{{url('/user/'.$user->id)}}">
                     <button class="btn btn-warning">Edit</button>
                     </a>
-                    <form action="{{url('/user')}}" method="post" style="display: inline;">
+                    <form action="{{url('/user')}}" method="post" style="display: inline;" onsubmit="return confirm_delete(this);">
                         @csrf
                         @method('delete')
                         <input type="hidden" name="id" value="{{$user->id}}">
@@ -49,7 +56,67 @@
           </ul>
         </div>
       </div>
+      <button class="btn" onclick="confirm_delete_click()">Click Me</button>
       <!-- /.card -->
     </div>
 </div>
+@endsection
+
+@section('scripts')
+    <script>
+        // ปุ่ม Click Me เอาไว้เช็ค
+        confirm_delete_click = function (){
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+                }).then(function(result) {
+                    console.log("Result",result)
+                    if (result.isConfirmed) {
+                        console.log("Delete It!",result)
+                    }
+                });
+            }
+
+            // ปุ่ม Delete เอาไว้ลบข้อมูล
+            function confirm_delete(form) {
+                event.preventDefault();
+                const swalWithBootstrapButtons = Swal.mixin({
+                    customClass: {
+                        confirmButton: "btn btn-success",
+                        cancelButton: "btn btn-danger"
+                    },
+                    buttonsStyling: false
+                });
+                swalWithBootstrapButtons.fire({
+                    title: "Are you sure?",
+                    text: "You won't be able to revert this!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonText: "Yes, delete it!",
+                    cancelButtonText: "No, cancel!",
+                    reverseButtons: true
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    swalWithBootstrapButtons.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    }).then(() => {
+                        form.submit();
+                    });
+                } else if ( result.dismiss === Swal.DismissReason.cancel) {
+                    swalWithBootstrapButtons.fire({
+                    title: "Cancelled",
+                    text: "Your imaginary file is safe :)",
+                    icon: "error"
+                    });
+                 }
+            });
+        }
+    </script>
 @endsection
